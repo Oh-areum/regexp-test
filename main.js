1e21e7242e7bab892aa79cb4c68591b956bd50fc
Oh-areum <dkfma309@naver.com>
2022-07-20 18:40:11 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,11 @@
+const str = `
+010-1234-5678
+thesecon@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c$s=frozen
+The quick vrown fox jumps over the lazy dog.
+abbcccddd
+d`
+
+console.log(
+    str.match(/./g)
+)
